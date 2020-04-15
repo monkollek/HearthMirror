6e0f82f46b03328689fa4c32fe13507f570b9a74
monkollek <monkol.lek@yale.edu>
2020-04-14 20:47:47 -0400

    Added more debug

--- HearthMirror/Mono/MonoClassField.cpp
+++ HearthMirror/Mono/MonoClassField.cpp
@@ -339,6 +339,8 @@ namespace hearthmirror {
                 MonoClass* arrClass = new MonoClass(_task, pArrClass, _is64bit);
                 MonoClass* elClass = new MonoClass(_task, ReadPointer(_task, pArrClass, _is64bit), _is64bit);
                 
+                printf("arrClass: %s elClass: %s\n", arrClass->getFullName().c_str(),elClass->getFullName().c_str())
+
                 printf("MonoClassField::ReadValue - 4\n");
                 uint32_t count = ReadInt32(_task, addr + (_is64bit ? kMonoArrayMaxLength64 : kMonoArrayMaxLength));
                 proc_address start = addr + (_is64bit ? kMonoArrayVector64 : kMonoArrayVector);
